2d4188ff43044858251e67457ff8c105d2e5e724
antyamok <65304976+antyamok@users.noreply.github.com>
2025-06-19 11:26:14 +0200

    Fix deployment configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: './',
   plugins: [react()],
+  base: '/',
   build: {
     outDir: 'dist',
     sourcemap: false,
@@ -22,4 +22,8 @@ export default defineConfig({
       overlay: false,
     },
   },
+  preview: {
+    port: 4173,
+    host: true
+  }
 });
